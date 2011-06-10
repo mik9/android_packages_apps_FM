1da633fb224ac6492d1a2f3b056e2cb7f94c7246
Tom Giordano <tomgiordano83@gmail.com>
2011-06-11 01:28:21 +1000

    FM: add overlay to set required bluetooth state
    The FM app currently turns on bluetooth when radio is started, however not all FM chips require bluetooth (eg. the siLabs chips used in ZTE and Samsung devices).
    Add an overlay to set whether bluetooth is required for FM.
    
    Change-Id: I8cc2810325f9fcc665c0e2a0e877aa840e58f489
    
    Conflicts:
    
    	res/values/config.xml

--- src/com/android/fm/radio/FMRadio.java
+++ src/com/android/fm/radio/FMRadio.java
@@ -162,7 +162,7 @@ public class FMRadio extends Activity {
     private Button[] mPresetButtons = {
             null, null, null, null, null
     };
-
+    private Context context;
     /* Middle row in the station info layout */
     private TextView mTuneStationFrequencyTV;
 
@@ -278,7 +278,7 @@ public class FMRadio extends Activity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
+	context = getApplicationContext();
         mPrefs = new FmSharedPreferences(this);
         mPrefs.Load();
         mCommandActive = CMD_NONE;
@@ -799,7 +799,11 @@ public class FMRadio extends Activity {
                 disableRadio();
             }
             else {
-                asyncCheckAndEnableRadio();
+		if (context.getResources().getBoolean(R.bool.require_bt)) {
+		    asyncCheckAndEnableRadio();
+		} else {
+		    enableRadio();
+		}
             }
             setTurnOnOffButtonImage();
         }
@@ -985,7 +989,8 @@ public class FMRadio extends Activity {
                 }
 
                 // Toggle BT on/off depending on value in preferences
-                toggleRadioOffBluetoothBehaviour();
+		if (context.getResources().getBoolean(R.bool.require_bt))
+		    toggleRadioOffBluetoothBehaviour();
             }
             catch (RemoteException e) {
                 Log.e(LOGTAG, "RemoteException in disableRadio", e);
@@ -1894,7 +1899,12 @@ public class FMRadio extends Activity {
                 try {
                     mService.registerCallbacks(mServiceCallbacks);
 
-                    asyncCheckAndEnableRadio();
+		    if (context.getResources().getBoolean(R.bool.require_bt)) {
+			asyncCheckAndEnableRadio();
+		    } else {
+			enableRadio();
+		    }
+
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
